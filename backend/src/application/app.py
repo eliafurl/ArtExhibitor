ad2bd72e09d4a3898bec64cac5acf3a936e1c5df
elia-furlanis <elia.furl@gmail.com>
2025-02-01 12:46:06 +0100

    Updated API port and allowed requests from frontend

--- backend/src/application/app.py
+++ backend/src/application/app.py
@@ -1,8 +1,12 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 from src.application.art_exhibitor import ArtExhibitor
 from src.utils.setup_env import OPENSEA_API_KEY
 
 app = Flask(__name__)
+
+# Allow requests from http://localhost:3000 (frontend)
+CORS(app, resources={r"/api/*": {"origins": "http://localhost:3000"}})
 
 # Initialize ArtExhibitor
 _art_exhibitor = ArtExhibitor(api_key=OPENSEA_API_KEY)
@@ -25,4 +29,4 @@ def home():
     return "ArtExhibitor is running!"
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5001, debug=True)
